10f77a843e8b6271f4832d2b732391407b72df6d
shalfi-sabit <shalfi.sabit@brainstation-23.com>
2026-07-15 17:57:36 +0600

    Imlemented the details for the blogs

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -15,8 +15,13 @@ export default async function BlogPage({ params }) {
   return (
     <article className='mt-10'>
       <div>
-        <h3 className='text-2xl font-semibold'>Blog {id} | </h3>
-        <p></p>
+        <h2 className='text-2xl font-semibold'>Blog {id} | </h2>
+        <h4 className='mt-4 font-semibold'>Lorem ipsum dolor sit amet.</h4>
+        
+        <div className='bg-red-200 mt-2 p-4'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iste provident eaque, vel repellendus enim placeat fugit neque quasi consequuntur quaerat magnam alias iusto voluptas numquam repellat nesciunt eum ullam assumenda laborum delectus magni doloribus! Reprehenderit sint ipsam rerum, laboriosam, odit, natus officia nihil earum sit fugiat mollitia quod ut?</p> 
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima minus quae veniam excepturi? Fugit fugiat delectus incidunt? Laboriosam magni atque, nobis aut ut hic voluptatum vel eaque voluptate illum, animi cumque? Minus, sunt natus nemo soluta ipsam molestias facere eligendi minima facilis, quas quasi sit sequi dignissimos est et earum?</p>
+        </div>
       </div>
     </article>
   )
