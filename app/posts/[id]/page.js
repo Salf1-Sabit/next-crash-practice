924f6f88de84fe237b6b809f6819446b96ed9461
shalfi-sabit <shalfi.sabit@brainstation-23.com>
2026-07-26 00:37:46 +0600

    Learnt about dynamic metadata

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,6 +1,16 @@
 import getPost from '@/lib/getPost';
 import React from 'react'
 
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+  const post = await getPost(id);
+
+  return {
+    title: post.title,
+    description: post.body
+  }
+}
+
 export default async function Post({ params }) {
   const { id } = await params;
   const post = await getPost(id);
